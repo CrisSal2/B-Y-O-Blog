4fe20ddc68a0cadaf34435f9a1cea25317cdeef9
CrisSal2 <salgado.chris.m@gmail.com>
2024-04-25 22:08:42 -0700

    Added back button and portfolio button

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -13,3 +13,37 @@ icon.onclick = function() {
     }
 
 }
+
+/*******************   Posts Local Storage Content *******************/
+
+const blogs = JSON.parse(localStorage.getItem("blogs"));
+
+const mainEl = document.querySelector("#blogs-container");
+
+for (let i = 0; i < blogs.length; i++) {
+    let blog = blogs[i];
+
+    const divEl = document.createElement("div");
+    const pEl = document.createElement("p");
+    const h2El = document.createElement("h2");
+    const h3El = document.createElement("h3");
+
+    h3El.textContent = blog.username;
+    h2El.textContent = blog.title;
+    pEl.textContent = blog.content;
+
+    divEl.append(h2El, h3El, pEl);
+    mainEl.append(divEl);
+
+    divEl.setAttribute('class', 'post');
+
+}
+
+/**********************************   Back Button       *******************/
+
+
+const backButton = document.getElementById("back-button");
+
+backButton.addEventListener('click', function () {
+    location.href = "index.html"
+});
